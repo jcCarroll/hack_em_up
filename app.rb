216c77b8d726fdbc4456ecc6713c285792d72629
2staffi@gmail.com <2staffi@gmail.com>
2017-09-01 20:34:40 -0400

    SQL statement to push to DB

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require 'sinatra'
+require 'pg'
+
+def connection
+    db_params = {
+      host: ENV['db'],
+      port:ENV['port'],
+      dbname:ENV['dbname'],
+      user:ENV['dbuser'],
+      password:ENV['dbpassword'],
+    }
+  
+    db = PG::Connection.new(db_params)
+end
 
 get '/' do
     erb :index
@@ -11,6 +24,9 @@ post '/add_book' do
     city = params[:city]
     state = params[:state]
     zipcode = params[:zipcode]
-    
+
+    db.exec ("INSERT INTO data (fname, lname, street, city, state, zipcode) VALUES ('#{fname}', '#{lname}', '#{address}','#{city}', '#{state}', '#{zipcode}')" )
+    db.close
+
     erb :index
 end
