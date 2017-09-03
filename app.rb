2bf35401dcedc9b49a377fbefe8aa91ea7abead7
2staffi@gmail.com <2staffi@gmail.com>
2017-09-03 09:56:30 -0400

    local file was named wrong

--- app.rb
+++ app.rb
@@ -1,17 +1,17 @@
 require 'sinatra'
 require 'pg'
 
-def connection
-    db_params = {
-      host: ENV['db'],
-      port:ENV['port'],
-      dbname:ENV['dbname'],
-      user:ENV['dbuser'],
-      password:ENV['dbpassword'],
-    }
-  
-    db = PG::Connection.new(db_params)
-end
+load "./local_env.rb" if File.exists?("./local_env.rb")
+
+db_params = {
+    host: ENV['dbhost'],
+    port: ENV['dbport'],
+    dbname: ENV['dbname'],
+    user: ENV['dbuser'],
+    password: ENV['dbpassword']
+	}
+
+db = PG::Connection.new(db_params)
 
 get '/' do
     erb :index
