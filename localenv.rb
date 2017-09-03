381ed363ca163884aa7bc84135d6a595803e6b91
2staffi@gmail.com <2staffi@gmail.com>
2017-09-03 09:00:55 -0400

    connection to DB is not going

---
+++ localenv.rb
@@ -0,0 +1,6 @@
+# -----DB Info-----
+ENV['host'] = 'mentormenteeapp.cemg8uvjfbhv.us-east-1.rds.amazonaws.com'
+ENV['port'] = '5432'
+ENV['dbname'] = 'devmentormentee'
+ENV['user'] = 'Tester'
+ENV['password'] = 'St0pl1gh7'
