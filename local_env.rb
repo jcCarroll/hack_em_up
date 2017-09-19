4aa305564a2000f60a972a61010fce69e6ee05c8
2staffi@gmail.com <2staffi@gmail.com>
2017-09-19 09:55:33 -0400

    testing eb deploy

---
+++ local_env.rb
@@ -0,0 +1,6 @@
+# -----DB Info-----
+ENV['dbhost'] = 'testhackapp.cp3bedzwvg7m.us-west-2.rds.amazonaws.com'
+ENV['dbport'] = '5432'
+ENV['dbname'] = 'TestHackDB'
+ENV['dbuser'] = 'Tester'
+ENV['dbpassword'] = 'St0pl1gh7'
